25180d022ebac70bfcec10c37679f094d33f171c
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-02-11 13:11:11 +0100

    Updates

---
+++ precache-manifest.ae0237a91d4b18d80b2dce4a2368e373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc45069dbf3c33e0bf82176f3354dc1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9276251e7764e7dfb806",
+    "url": "/monsters-rolodex/static/css/main.b249ea13.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9276251e7764e7dfb806",
+    "url": "/monsters-rolodex/static/js/main.37f72ea5.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
